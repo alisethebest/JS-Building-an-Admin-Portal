634733c0c4efe989653bc8ad88427edec3703eb2
Frank Cervantes <109487989+alisethebest@users.noreply.github.com>
2023-06-14 12:54:47 -0700

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,31 @@
 
 // Your Code Here
+const bookId = "12345"; // Replace with the actual book ID
+const newTitle = "New Book Title"; // Replace with the new title
+
+const url = `/updateBook`;
+const body = {
+  id: bookId,
+  title: newTitle,
+};
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+const requestOptions = {
+  method: "PATCH",
+  headers,
+  body: JSON.stringify(body),
+};
+
+fetch(url, requestOptions)
+  .then((response) => response.json())
+  .then((updatedBook) => {
+    // Handle the updated book object
+    console.log(updatedBook);
+  })
+  .catch((error) => {
+    // Handle the error
+    console.error("Error:", error);
+  });
